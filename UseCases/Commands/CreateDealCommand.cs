c064b946d641816f2f0364f2d83326ea5e9d0e3c
Haotit <rhjdfdfZfyz@gmail.com>
2025-10-23 23:12:10 +0700

    Deal - DealCommand DealHendler IDealRepositor

---
+++ UseCases/Commands/CreateDealCommand.cs
@@ -0,0 +1,13 @@
+using Domain.Domain.Deal;
+using Domain.Domain.ValueObjects;
+
+namespace UseCases.Commands
+{
+    public class CreateDealCommand : ICommand<Deal>
+    {
+        public ClientId ClientId { get; set; }
+        public PropertyId PropertyId { get; set; }
+        public BookingId? BookingId { get; set; }
+        public DealDetails Details { get; set; }
+    }
+}
