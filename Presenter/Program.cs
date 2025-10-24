b4c4b569c3879cf5cd5495d78fd9416430ad046c
Haotit <rhjdfdfZfyz@gmail.com>
2025-10-24 21:19:05 +0700

    Практика

--- Presenter/Program.cs
+++ Presenter/Program.cs
@@ -1,7 +1,27 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Presenter;
+using Domain.Database;
+using Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// // Получаем строку подключения из конфигурации
+// var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+//                       "Server=(localdb)\\mssqllocaldb;Database=EstateManagementDb;Trusted_Connection=true;MultipleActiveResultSets=true;";
+
+PostgreSqlConnectionOptions? options = builder.Configuration.GetSection(nameof(PostgreSqlConnectionOptions)).Get<PostgreSqlConnectionOptions>();
+
+if (options == null)
+{
+    throw new InvalidOperationException("PostgreSqlConnectionOptions not found");
+}
+
+Console.WriteLine(options.HostName);
+Console.WriteLine(options.DatabaseName);
+Console.WriteLine(options.Username);
+Console.WriteLine(options.Password);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
 app.Run();
+
