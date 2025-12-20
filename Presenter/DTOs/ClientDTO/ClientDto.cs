d364230d8200dc691f59e452e2608960a95d07ba
Haotit <rhjdfdfZfyz@gmail.com>
2025-12-21 03:38:30 +0700

    Правки Client

--- Presenter/DTOs/ClientDTO/ClientDto.cs
+++ Presenter/DTOs/ClientDTO/ClientDto.cs
@@ -7,50 +7,17 @@ namespace Presenter.DTOs.ClientDTO
     /// </summary>
     public class ClientDto
     {
-        /// <summary>
-        /// Идентификатор клиента
-        /// </summary>
-        [Required(ErrorMessage = "Идентификатор клиента обязателен")]
         public Guid Id { get; set; }
 
-        /// <summary>
-        /// Имя клиента
-        /// </summary>
-        [Required(ErrorMessage = "Имя обязательно")]
-        [StringLength(200, MinimumLength = 2, ErrorMessage = "Имя должно содержать от 2 до 200 символов")]
-        public string FirstName { get; set; }
+       public string FirstName { get; set; }
 
-        /// <summary>
-        /// Фамилия клиента
-        /// </summary>
-        [Required(ErrorMessage = "Фамилия обязательна")]
-        [StringLength(200, MinimumLength = 2, ErrorMessage = "Фамилия должна содержать от 2 до 200 символов")]
         public string LastName { get; set; }
 
-        /// <summary>
-        /// Email клиента
-        /// </summary>
-        [Required(ErrorMessage = "Email обязателен")]
-        [EmailAddress(ErrorMessage = "Некорректный формат email")]
-        [StringLength(100, ErrorMessage = "Email не может превышать 100 символов")]
         public string Email { get; set; }
 
-        /// <summary>
-        /// Номер телефона клиента
-        /// </summary>
-        [Required(ErrorMessage = "Номер телефона обязателен")]
-        [RegularExpression(@"^(\+7|8)(\s|-|\()?([0-9]{3})(\s|-|\))?([0-9]{3})(\s|-)?([0-9]{2})(\s|-)?([0-9]{2})$",
-            ErrorMessage = "Некорректный формат российского номера телефона")]
         public string PhoneNumber { get; set; }
 
-        /// <summary>
-        /// Дата регистрации
-        /// </summary>
         public DateTime RegisteredDate { get; set; }
-
-        /// <summary>
-        /// Дата последнего обновления
-        /// </summary>
         public DateTime? UpdatedAt { get; set; }
     }
 }
--- UseCases/Property/CreatePropertyCommand.cs
+++ UseCases/Property/CreatePropertyCommand.cs
@@ -5,12 +5,11 @@ using UseCases.Interfaces.Commands;
 
 namespace UseCases.Property
 {
-    public class CreatePropertyCommand : ICommand<PropertyEntity>
-    {
-        public Address Address { get; set; }
-        public Price Price { get; set; }
-        public Description Description { get; set; }
-        public PropertyDetails Details { get; set; }
-        public OwnershipRecord OwnerRecord { get; set; }
-    }
+    public record CreatePropertyCommand(
+        Address Address,
+        Price Price,
+        Description Description,
+        PropertyDetails Details,
+        OwnershipRecord OwnerRecord
+    ) : ICommand<PropertyEntity>;
 }
